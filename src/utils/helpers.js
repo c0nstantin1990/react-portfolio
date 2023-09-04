bf74ff4ee21058570c40c6ebfbbd147ac511550d
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-09-04 18:23:54 -0400

    adding utils for email validation & capitalize

---
+++ src/utils/helpers.js
@@ -0,0 +1,8 @@
+export function capitalizeFirstLetter(string) {
+  return string ? string.charAt(0).toUpperCase() + string.slice(1) : "";
+}
+
+export function validateEmail(email) {
+  const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+  return emailRegex.test(email);
+}
